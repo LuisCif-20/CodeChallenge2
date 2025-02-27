12590425ab806d0b9458cebef2b63f988b2663f5
LuisCif-20 <n.rodas225@gmail.com>
2025-02-27 17:31:15 -0600

    Complexity added

--- src/main/java/org/example/CC3/Department.java
+++ src/main/java/org/example/CC3/Department.java
@@ -7,6 +7,11 @@ public class Department {
 
     private Map<Student, Integer> studentsMarks = new HashMap<Student,Integer>();
     
+    /*
+     * Complejidad O(n) ya que recorre el mapa de las notas de los estudiantes, verifica si
+     * existe un estudiante con el mismo apellido, de ser asi, elimina ese entry y lo agrega al mapa,
+     * si no cumple la condicion solo lo agrega al mapa.
+     */
     public void addStudent(final Student student, final int mark) {
         for (Map.Entry<Student, Integer> studentMark : studentsMarks.entrySet()) {
             if (studentMark.getKey().getLastName().equals(student.getLastName())) {
